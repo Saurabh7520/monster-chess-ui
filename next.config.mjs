2110ccb0131d8477785f7468cc30d1da84824cb3
Saurabh Raj <saurabh752096@gmail.com>
2026-07-02 21:13:07 +0530

    Bypass ESLint check for Vercel deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    // This tells Vercel to build the site even if TypeScript throws a warning
     ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
   },
 };
 
